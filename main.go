05a7d629eee0548990baca6caa73787c41b99681
Nitisit Sooksang <nitisit.sooksang@sscinc.com>
2026-04-23 14:31:52 +0700

    update core to support frontend.

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	// Middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173,http://localhost:5174,http://localhost:3000,https://idlegame-backend--popomut.replit.app/",
+		AllowOrigins:     "http://localhost:5173,http://localhost:5174,http://localhost:3000,https://idlegame-frontend--popomut.replit.app",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
